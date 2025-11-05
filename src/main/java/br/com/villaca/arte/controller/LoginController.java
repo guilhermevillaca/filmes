c453c90797944fcea02272d04e16591ff0c2b9c6
Guilherme Villaca <guidvillaca@gmail.com>
2025-11-05 16:09:39 -0300

    add login

--- src/main/java/br/com/villaca/arte/controller/LoginController.java
+++ src/main/java/br/com/villaca/arte/controller/LoginController.java
@@ -1,13 +1,15 @@
 package br.com.villaca.arte.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.villaca.arte.model.Login;
 import br.com.villaca.arte.model.Usuario;
 import br.com.villaca.arte.repository.UsuarioRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -19,13 +21,15 @@ public class LoginController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-     @PostMapping("/entrar")
-    public ResponseEntity<String> entrar(@RequestBody Usuario usuario) {
+    @PostMapping("/entrar")
+    public ResponseEntity<Object> entrar(@RequestBody Login usuario) {
+        System.out.println(usuario.getLogin());
+        System.out.println(usuario.getSenha());
 
-        Usuario usuarioIn = usuarioRepository.findByLogin(usuario.getLogin());
-
-        if (usuarioIn.equals(usuario.getLogin()) && usuarioIn.equals(usuario.getSenha())) {
-            return ResponseEntity.ok("Login realizado com sucesso!");
+        Optional<Usuario> usuarioIn = usuarioRepository.findByLogin("guilherme.villaca");
+        System.out.println(usuarioIn.get().getLogin());
+        if (usuarioIn.isPresent() && usuarioIn.get().equals(usuario.getLogin()) && usuarioIn.get().equals(usuario.getSenha())) {
+            return new ResponseEntity<>(usuarioIn.get(), HttpStatus.OK);
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha inválidos");
     }
--- src/main/java/br/com/villaca/arte/repository/UsuarioRepository.java
+++ src/main/java/br/com/villaca/arte/repository/UsuarioRepository.java
@@ -1,13 +1,15 @@
 package br.com.villaca.arte.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.villaca.arte.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
-    Usuario findByLogin(String login);
+    Optional<Usuario> findByLogin(String login);
 
 }
