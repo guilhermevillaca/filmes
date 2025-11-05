c28685114079edee1024a448121c9438d31f39b2
Guilherme Villaca <guidvillaca@gmail.com>
2025-11-05 16:16:42 -0300

    correção logincontroller

--- src/main/java/br/com/villaca/arte/controller/LoginController.java
+++ src/main/java/br/com/villaca/arte/controller/LoginController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.villaca.arte.model.Login;
 import br.com.villaca.arte.model.Usuario;
 import br.com.villaca.arte.repository.UsuarioRepository;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "login")
@@ -22,13 +23,9 @@ public class LoginController {
     UsuarioRepository usuarioRepository;
 
     @PostMapping("/entrar")
-    public ResponseEntity<Object> entrar(@RequestBody Login usuario) {
-        System.out.println(usuario.getLogin());
-        System.out.println(usuario.getSenha());
-
-        Optional<Usuario> usuarioIn = usuarioRepository.findByLogin("guilherme.villaca");
-        System.out.println(usuarioIn.get().getLogin());
-        if (usuarioIn.isPresent() && usuarioIn.get().equals(usuario.getLogin()) && usuarioIn.get().equals(usuario.getSenha())) {
+    public ResponseEntity<Object> entrar(@RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioIn = usuarioRepository.findByLogin(usuario.getLogin());        
+        if (usuarioIn.isPresent() && usuarioIn.get().getLogin().equals(usuario.getLogin()) && usuarioIn.get().getSenha().equals(usuario.getSenha())) {
             return new ResponseEntity<>(usuarioIn.get(), HttpStatus.OK);
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha inválidos");
